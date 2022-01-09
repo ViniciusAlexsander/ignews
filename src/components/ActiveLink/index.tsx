f7bb48bfeaa89b19a946b30afa317489c3443541
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-01-08 22:40:50 -0300

    adicionando cms

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,20 @@
+import { ReactElement, cloneElement } from "react";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  activeClassName,
+  children,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return <Link {...rest}>{cloneElement(children, { className })}</Link>;
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -7,10 +11,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo Ig.News" />
         <nav>
-          <a className={styles.active} href="/">
-            Home
-          </a>
-          <a href="/">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
